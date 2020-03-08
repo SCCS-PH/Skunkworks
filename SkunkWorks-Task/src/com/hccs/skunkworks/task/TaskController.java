1deab96be7306aac939533ffc7f5b83848705cab
Denross Salenga <denrosssalenga@gmail.com>
2020-03-08 22:56:17 +0800

    Renaming Model to Task

--- SkunkWorks-Task/src/com/hccs/skunkworks/task/TaskController.java
+++ SkunkWorks-Task/src/com/hccs/skunkworks/task/TaskController.java
@@ -21,31 +21,37 @@ public enum TaskController {
         TaskBean dw = new TaskBean();
         dw.setTaskName("Sample A");
         dw.setTaskValue(1);
+        dw.setActive(true);
         works.add(dw);
 
         dw = new TaskBean();
         dw.setTaskName("Sample B");
         dw.setTaskValue(2);
+        dw.setActive(true);
         works.add(dw);
 
         dw = new TaskBean();
         dw.setTaskName("Sample C");
         dw.setTaskValue(4);
+        dw.setActive(true);
         works.add(dw);
 
         dw = new TaskBean();
         dw.setTaskName("Sample D");
         dw.setTaskValue(8);
+        dw.setActive(true);
         works.add(dw);
 
         dw = new TaskBean();
         dw.setTaskName("Sample E");
         dw.setTaskValue(0x10);
+        dw.setActive(true);
         works.add(dw);
 
         dw = new TaskBean();
         dw.setTaskName("Sample F");
         dw.setTaskValue(0x20);
+        dw.setActive(true);
         works.add(dw);
 
         return works;
